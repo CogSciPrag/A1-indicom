801886b676e873dd72b0af2adac1cac5231eeedc
Amelie Rueeck <amelie.rueeck@web.de>
2026-04-20 09:24:45 +0200

    change experiment to prolific setting

--- experiments/A1-Indicom-pilot3/src/magpie.config.js
+++ experiments/A1-Indicom-pilot3/src/magpie.config.js
@@ -3,10 +3,10 @@ export default {
   serverUrl: 'https://magpie-cogsciprag.fly.dev',
   socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',
   // this will be used in prolific mode
-  completionUrl: 'https://app.prolific.com/submissions/complete?cc=C1JKMUV9',
+  completionUrl: 'https://app.prolific.com/submissions/complete?cc=C15H53J3',
   contactEmail: 'michael.franke@uni-tuebingen.de',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'directLink',
+  mode: 'prolific',
   language: 'en'
 };
  
